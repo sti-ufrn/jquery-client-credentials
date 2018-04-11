9fa1878cbb0bcda5796ba2f5008bfd4b8e83fe1d
GusttavoHenrique <gust.tavohenrique@hotmail.com>
2018-04-11 12:03:31 -0300

    Mudando as URLs de api e autenticação testes

--- jquery-client-credentials.js
+++ jquery-client-credentials.js
@@ -1,35 +1,37 @@
-var client_id = "my_client_id";
-var client_secret = "my_client_secret";
-var x_api_key = "my_x_api_key";
+var url_base_autenticacao = "https://autenticacao.info.ufrn.br/";
+var client_id = "<my_client_id>";
+var client_secret = "<my_client_secret>";
+
 var url_base = "https://api.info.ufrn.br/";
-var url_token = "https://autenticacao.info.ufrn.br/authz-server/oauth/token?client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=" + "client_credentials";
+var versao = "<versao_api>";
+var x_api_key = "<my_x_api_key>";
 
 var settings = {  
-	"async": true,  
-	"crossDomain": true,  
-	"url": url_token,  
-	"method": "POST"
+  "async": true,  
+  "crossDomain": true,  
+  "url": url_base_autenticacao + "authz-server/oauth/token?client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials",  
+  "method": "POST"
 }
 
 var token = null;
 
 $.ajax(settings).done(function (response) {  
-	token = response.access_token;
+  token = response.access_token;
 
-	var settings = {  
+  var settings = {  
         "async": true,  
         "crossDomain": true, 
-        "url": url_base + "curso/v0.1/cursos",
-		"headers": {
-		    "Authorization": "bearer " + token,
-		    "x-api-key": x_api_key
-		},  
+        "url": url_base + "curso/" + versao + "/cursos",
+    "headers": {
+        "Authorization": "bearer " + token,
+        "x-api-key": x_api_key
+    },  
         "method": "GET"
-	}
+  }
 
     $.ajax(settings).done(function (response) {  
         console.log(response);
     }).fail(function (e) {
-		console.log(e);
-	});
+    console.log(e);
+  });
 });
